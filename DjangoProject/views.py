ed24db113ed420d96100d5d4beb104a358cd9066
Ülvü Süleymanov <ulvu.suleymanov@kapitalbank.az>
2025-09-13 15:50:57 +0400

    feat: add questions list page

--- DjangoProject/views.py
+++ DjangoProject/views.py
@@ -7,4 +7,5 @@ from question.models import Question
 @login_required(login_url='/auth/login')
 def index(request):
     questions = Question.objects.prefetch_related('variants').all().order_by('-created_at')
+
     return render(request, 'index.html', {'questions': questions})
